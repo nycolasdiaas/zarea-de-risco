c99e6021f16da1a85337b730ea87af03b4e97ac1
Luiz Carlos Filho <lcarlos.mln@gmail.com>
2024-12-16 21:29:00 -0300

    feat: restructure App component; add MainSection, Neighborhood, RecentNews, ToolBar, and Footer components

--- frontend/Zarea_De_Risco_FrontEnd/src/components/Footer/index.jsx
+++ frontend/Zarea_De_Risco_FrontEnd/src/components/Footer/index.jsx
@@ -1,26 +1,11 @@
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-
-
-function BrandExample() {
+function Footer() {
   return (
-    
-      <Navbar sticky="top" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src="../../../public/imgs/AZAREA2 1.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{' '}
-            React Bootstrap
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-    
+    <>
+      <br />
+      Footer
+      <br />
+    </>
   );
 }
 
-export default BrandExample;
+export default Footer;
