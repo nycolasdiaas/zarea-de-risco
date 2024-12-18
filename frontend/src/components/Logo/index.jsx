c3cd511b569a30599411cc13dfddd87fcafa6f67
Luiz Carlos Filho <lcarlos.mln@gmail.com>
2024-12-17 23:43:50 -0300

    Setting Up
    feat: set up React with Vite and Bootstrap, add Navbar component

---
+++ frontend/src/components/Logo/index.jsx
@@ -0,0 +1,14 @@
+function Logo() {
+  return (
+    <a className="navbar-brand img-fluid" href="#">
+      <img
+        src="/imgs/AZAREA2 1.png"
+        alt="Logotipo"
+        id="imglogo"
+        className="m-0 m-md-3 ms-md-5"
+      />
+    </a>
+  );
+}
+
+export default Logo;
