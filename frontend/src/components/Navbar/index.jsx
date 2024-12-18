c42588df8f7a461eb5af9c10ff610f32cfc74dcc
Luiz Carlos Filho <lcarlos.mln@gmail.com>
2024-12-17 23:43:50 -0300

    Setting Up
    feat: set up React with Vite and Bootstrap, add Navbar component

---
+++ frontend/src/components/Navbar/index.jsx
@@ -0,0 +1,16 @@
+import Links from "../Links";
+import Logo from "../Logo";
+
+function Navbar() {
+  return (
+    <nav id="navbar" className="navbar navbar-expand-lg sticky-top">
+      <div className="container-fluid d-flex">
+        <Logo />
+        <h2 className="text-sm-start">Lorem ipsum dolor</h2>
+        <Links />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
