6c5759fe967c1aef06c8adce7c2c8db69176210d
welli7ngton <welli7ngton@zorin>
2024-12-21 01:44:30 -0300

    enh: refactor the code and centralize the use of the env vars - create a super class to services

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api
-from .handle_minio.resources import GetAllMetadata, GetMetadataFromDate
+from .handle_minio.resources import GetDailyMetadata, GetMetadataFromDate
 
 
 def create_app():
@@ -11,7 +11,7 @@ def create_app():
     def home():
         return "Hello, World!"
 
-    api.add_resource(GetAllMetadata, "/get_all_metadata")
+    api.add_resource(GetDailyMetadata, "/get_daily_metadata")
     api.add_resource(GetMetadataFromDate, "/get_metadata_from_date/<string:date>")
 
     return app
