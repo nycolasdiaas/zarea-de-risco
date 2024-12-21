d48c652566d873a458333bdc00c9ab455409767d
Wellington Almeida <wellingtonasilva45@gmail.com>
2024-12-21 01:44:30 -0300

    enh: refactor the code and centralize the use of the env vars - create a super class to services

--- backend/handle_minio/services.py
+++ backend/handle_minio/services.py
@@ -1,40 +1,44 @@
-from os import getenv
-from dotenv import load_dotenv
 from .minio_generic_client.minio_client import MinioClient
 from urllib3.response import BaseHTTPResponse
 from urllib3.exceptions import MaxRetryError
 
-load_dotenv()
-
 
 class MetadataService:
-    def get_all_metadata(self) -> BaseHTTPResponse:
+    def __init__(
+        self,
+        endpoint: str,
+        access_key: str,
+        secret_key: str,
+        bucket_name: str,
+        secure: bool = False,
+    ) -> None:
+        self.__service__ = MinioClient(
+            endpoint=endpoint,
+            access_key=access_key,
+            secret_key=secret_key,
+            secure=secure,
+        )
+
+        self.bucket_name = bucket_name
+
+    # TODO: need to create the minio path to be used by this function.
+    def get_daily_metadata(self) -> BaseHTTPResponse:
         try:
-            client = MinioClient(
-                endpoint=getenv("MINIO_BACKEND_ENDPOINT") or "MINIO_BACKEND_ENDPOINT",
-                access_key=getenv("MINIO_ACCESS_KEY"),
-                secret_key=getenv("MINIO_SECRECT_KEY"),
-                secure=False,
-            ).get_client()
+            client = self.__service__.get_client()
 
             return client.get_object(
-                bucket_name=getenv("MINIO_BUCKET_NAME", "MINIO_BUCKET_NAME not found."),
-                object_name="Portalnoticiasceara/2024-12-14/metadata.json",
+                bucket_name=self.bucket_name,
+                object_name="add_daily_data_path_in_minio",
             )
         except MaxRetryError as e:
             raise Exception(f"Máximo de tentativas atingido: {e}")
 
     def get_metadata_from_date(self, date: str) -> BaseHTTPResponse:
         try:
-            client = MinioClient(
-                endpoint=getenv("MINIO_BACKEND_ENDPOINT") or "MINIO_BACKEND_ENDPOINT",
-                access_key=getenv("MINIO_ACCESS_KEY"),
-                secret_key=getenv("MINIO_SECRECT_KEY"),
-                secure=False,
-            ).get_client()
+            client = self.__service__.get_client()
 
             return client.get_object(
-                bucket_name=getenv("MINIO_BUCKET_NAME", "MINIO_BUCKET_NAME not found."),
+                bucket_name=self.bucket_name,
                 object_name=f"Portalnoticiasceara/{date}/metadata.json",
             )
         except MaxRetryError as e:
