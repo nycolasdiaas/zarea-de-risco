7bbd7bdb1f17c31672a954561f9c045c2ee2acc5
nycolasdiaas <nycolaskate2003@hotmail.com>
2025-01-02 09:29:30 -0300

    feat/fix_base-output-folder

--- data_processing/jobs/telegram_downloader.py
+++ data_processing/jobs/telegram_downloader.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 API_ID = int(os.getenv("TELEGRAM_API_ID", 0))
 API_HASH = os.getenv("TELEGRAM_API_HASH", "None")
-BASE_OUTPUT_FOLDER = "./downloads"
+BASE_OUTPUT_FOLDER = os.getenv('BASE_OUTPUT_FOLDER', './downloads')
 DAYS_OFFSET = int(os.getenv("DAYS_OFFSET", 0))
 MIN_DATE = os.getenv("MIN_DATE", None)
 MAX_DATE = os.getenv("MAX_DATE", None)
