6668f6ff1101f04446e6efc96e61faabf366d968
Luiz Carlos Filho <lcarlos.mln@gmail.com>
2025-01-05 20:36:51 -0300

    feat: update navbar layout and styles, add search bar component

--- frontend/src/components/SearchBar/index.jsx
+++ frontend/src/components/SearchBar/index.jsx
@@ -1,16 +1,16 @@
-function SearchBar(){
-    return (
-        <div className="col-md-4 d-flex align-self-end">
-        <div className="search-container align-self-end m-3 mgr2" id="">
-          <input
-            type="text"
-            className="form-control search-input"
-            placeholder="Search..."
-          />
-          <i className="fas fa-search search-icon"></i>
-        </div>
+function SearchBar() {
+  return (
+    <div className="col-md-4 d-flex align-self-end">
+      <div className="search-container m-3 mgr2" id="searchBar">
+        <input
+          type="text"
+          className="form-control search-input"
+          placeholder="Search..."
+        />
+        <i className="fas fa-search search-icon"></i>
       </div>
-    );
+    </div>
+  );
 }
 
 export default SearchBar;
