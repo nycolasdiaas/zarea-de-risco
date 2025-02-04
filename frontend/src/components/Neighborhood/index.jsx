871694c8033cb5b6f12c19782dd7da74520eed3c
Luiz Carlos Filho <lcarlos.mln@gmail.com>
2025-02-04 12:54:51 -0300

    General changes in css and components
    changing css and modifying components to a better responsivity

--- frontend/src/components/Neighborhood/index.jsx
+++ frontend/src/components/Neighborhood/index.jsx
@@ -2,82 +2,82 @@ function Neighborhood() {
     return (
       <div className="container featured">
         <h1 className="Featured_MainTitle">Procure por bairros</h1>
-          <div className="row d-flex flex-wrap text-center justify-content-center">
+          <div className="d-flex flex-wrap text-center justify-content-center">
             <div className="col-md col-sm featured_container">
               <img
-                className="fadeInUp-bairros rounded-3"
+                className="bairros bairros fadeInUp rounded-3"
                 src="/imgs/Aerolandia.png"
                 alt="Imagem"
               />
               
-              <div className="AnimatedText">
+              {/* <div className="AnimatedText d-flex flex-wrap justify-content-center">
                 <h3 className="featured_news_title">Aerolandia</h3>
-              </div>
+              </div> */}
             </div>
   
             <div className="col-md col-sm featured_container">
               <img
-                className="fadeInUp-bairros rounded-3"
+                className="bairros fadeInUp rounded-3"
                 src="/imgs/Cajazeiras.png"
                 alt="Imagem"
               />
-              <div className="AnimatedText">
+              {/* <div className="AnimatedText">
                 <h3 className="featured_main_title">Cajazeiras</h3>
-              </div>
+              </div> */}
             </div>
           
             <div className="col-md col-sm featured_container">
               <img
-                className="fadeInUp-bairros rounded-3"
+                className="bairros fadeInUp rounded-3"
                 src="/imgs/Cambeba.png"
                 alt="Imagem"
               />
           
               <div className="AnimatedText">
-                <h3 className="featured_main_title">Cambeba</h3>
+                {/* <h3 className="featured_main_title">Cambeba</h3> */}
               </div>
             </div>
             <div className="col-md col-sm featured_container">
               <img
-                className="fadeInUp-bairros rounded-3"
+                className="bairros fadeInUp rounded-3"
                 src="/imgs/Edson_Queiroz.png"
                 alt="Imagem"
               />
               <div className="AnimatedText">
-                <h3 className="featured_main_title">Edson Queiroz</h3>
+                {/* <h3 className="featured_main_title">Edson Queiroz</h3> */}
               </div>
             </div>
 
             <div className="col-md col-sm featured_container">
               <img
-                className="fadeInUp-bairros rounded-3"
+                className="bairros fadeInUp rounded-3"
                 src="/imgs/Jose_de_Alencar.png"
                 alt="Imagem"
               />
               <div className="AnimatedText">
-                <h3 className="featured_main_title">Jose de Alencar</h3>
+                {/* <h3 className="featured_main_title">Jose de Alencar</h3> */}
               </div>
             </div>
 
             <div className="col-md col-sm featured_container">
               <img
-                className="fadeInUp-bairros rounded-3"
+                className="bairros fadeInUp rounded-3"
                 src="/imgs/Lagoa_Sapiranga.png"
                 alt="Imagem"
               />
               <div className="AnimatedText">
-                <h3 className="featured_main_title">Lagoa Sapiranga</h3>
+                {/* <h3 className="featured_main_title">Lagoa Sapiranga</h3> */}
               </div>
             </div>
 
             <div className="col-md col-sm featured_container">
               <img
-                className="fadeInUp-bairros rounded-3"
+                className="bairros fadeInUp rounded-3"
                 src="/imgs/Parque_Iracema.png"
                 alt="Imagem"
               />
               <div className="AnimatedText">
-                <h3 className="featured_main_title">Parque Iracema</h3>
+                {/* <h3 className="featured_main_title">Parque Iracema</h3> */}
               </div>
             </div>
             
