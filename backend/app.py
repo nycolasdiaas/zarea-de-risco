dc0d52f6330f8620115529196a4ba4278b13b42c
welli7ngton <wellingtonasilva45@gmail.com>
2025-03-03 20:51:08 -0300

    fix: error in endpoint names

--- backend/app.py
+++ backend/app.py
@@ -15,13 +15,13 @@ def create_app():
     api = Api(app)
 
     api.add_resource(Home, "/")
-    api.add_resource(GetDailyMetadata, "metadata/daily")
-    api.add_resource(GetWeeklyMetadata, "metadata/weekly")
-    api.add_resource(GetMonthlyMetadata, "metadata/monthly")
-    api.add_resource(GetMetadataFromDate, "metadata/date/<string:date>")
+    api.add_resource(GetDailyMetadata, "/metadata/daily")
+    api.add_resource(GetWeeklyMetadata, "/metadata/weekly")
+    api.add_resource(GetMonthlyMetadata, "/metadata/monthly")
+    api.add_resource(GetMetadataFromDate, "/metadata/date/<string:date>")
     api.add_resource(
         GetMetadataFromLocation,
-        "metadata/location/<string:state>/<string:city>/<string:neigborhood>/<string:street>",
+        "/metadata/location/<string:state>/<string:city>/<string:neigborhood>/<string:street>",
     )
 
     return app
